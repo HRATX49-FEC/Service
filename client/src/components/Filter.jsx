4a240dd2f0b7d4e5f6c868f19247296f5ad97232
Peyton Perret <kpeyton95@gmail.com>
2020-07-06 14:36:17 -0500

    added functionality to filter reviews by rating

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -10,16 +10,8 @@ const Filter = (props) => {
                 {props.sortTypes.map((e, i) => { return ( <option key={i} value={e}>sort by {e}</option> )})}
             </select>
             <select id="filterSelector" onChange={(e) => props.filter(e)}>
-                {[...Array(5)].map((n, i) => { return (<option key={i} value={n}>filter by {i} stars</option> )})}
+                {[...Array(6)].map((n, i) => { return ( <option key={i} value={i}>filter by {i} stars</option> )}).reverse()}
             </select>
-          {/* <button>sort by <span>{props.sortBy}</span></button>
-          <button onChange={(e) => props.filterByRating(e)}>filter by <span>5 stars</span></button>
-            <div className="sortList">
-            {props.sortTypes.map((e, i)=> { return (<ul className="sortListItem" key={i} value={e} onChange={(e) => props.sort(e)}>{e}</ul>)})}
-            </div>
-            <div className="sortList">
-            {props.filterTyes.map((e, i)=> { return (<ul className="sortListItem" key={i} value={e} onChange={(e) => props.sort(e)}>{e}</ul>)})}
-            </div> */}
           </form>
         </div>
       </>
